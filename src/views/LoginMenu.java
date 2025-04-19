fdf8eb22579a665e96edce6c7976a413fa024afa
Amirhossein2006-hub <a.h.ayoubian@gmail.com>
2025-04-19 22:00:25 +0330

    Login menu view completed

--- src/views/LoginMenu.java
+++ src/views/LoginMenu.java
@@ -1,10 +1,63 @@
 package views;
 
+import java.util.regex.Matcher;
+
+import controllers.LoginMenuController;
+import models.enums.Commands.LoginMenuCommands;
+
 public class LoginMenu implements AppMenu {
     @Override
     public void check(String command) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'check'");
+        Matcher matcher;
+
+        matcher = LoginMenuCommands.MENU_ENTER.regexMatcher(command);
+        if (matcher.matches()) {
+            String menuName = matcher.group("menuName");
+
+            LoginMenuController.ChangeMenu(menuName);
+            return;
+        }
+
+        matcher = LoginMenuCommands.MENU_EXIT.regexMatcher(command);
+        if (matcher.matches()) {
+            LoginMenuController.Exit();
+            return;
+        }
+
+        matcher = LoginMenuCommands.SHOW_CURRENT_MENU.regexMatcher(command);
+        if (matcher.matches()) {
+            LoginMenuController.ShowCurrentMenu();
+            return;
+        }
+        
+        matcher = LoginMenuCommands.LOGIN.regexMatcher(command);
+        if (matcher.matches()) {
+            String username = matcher.group("username");
+            String password = matcher.group("password");
+            String stayLoggedIn = matcher.group("stayLoggedIn");
+            boolean stayLoggedInBoolean;
+            if (stayLoggedIn == null) stayLoggedInBoolean = false;
+            else stayLoggedInBoolean = true;
+
+            LoginMenuController.Login(username, password, stayLoggedInBoolean);
+            return;
+        }
+
+        matcher = LoginMenuCommands.FORGET_PASSWORD.regexMatcher(command);
+        if (matcher.matches()) {
+            String username = matcher.group("username");
+
+            LoginMenuController.ForgotPassword(username);
+            return;
+        }
+
+        // TODO answer bayad dakhel forgot-password piadesazi shavad
+
+        matcher = LoginMenuCommands.USER_LOGOUT.regexMatcher(command);
+        if (matcher.matches()) {
+            // LoginMenuController.Logout();
+            return;
+        }
     }
     
     @Override
