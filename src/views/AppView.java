764b500b8b70435241a004f8f198de11c17c62f4
Amirhossein2006-hub <a.h.ayoubian@gmail.com>
2025-04-19 21:09:56 +0330

    AppView changed

--- src/views/AppView.java
+++ src/views/AppView.java
@@ -3,13 +3,14 @@ package views;
 import java.util.Scanner;
 
 import models.App;
+import models.enums.Commands.Menus;
 
 public class AppView {
     public static void run() {
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
 
-        while (command != "exit") {
+        while (!App.getCurrentMenu().equals(Menus.ExitMenu)) {
             App.getCurrentMenu().checkCommand(command);
 
             command = scanner.nextLine();
