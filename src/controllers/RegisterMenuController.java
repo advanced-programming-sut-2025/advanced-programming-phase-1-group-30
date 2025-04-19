159bb51e13676e268826279008c93f39d91444f9
Amirhossein2006-hub <a.h.ayoubian@gmail.com>
2025-04-19 20:46:30 +0330

    Register Menu View completed

--- src/controllers/RegisterMenuController.java
+++ src/controllers/RegisterMenuController.java
@@ -1,8 +1,16 @@
 package controllers;
 
 public class RegisterMenuController {
-    public static void register(String username, String password, String repeatPassword, String nickName, String email, String gender) {}
-    public static void ChangeMenu(String menuName) {}
-    public static void ShowCurrentMenu() {}
-    public static void Exit() {}
+    public static String register(String username, String password, String repeatPassword, String nickName, String email, String gender) {
+        return "";
+    }
+    public static String changeMenu(String menuName) {
+        return "";
+    }
+    public static String showCurrentMenu() {
+        return "";
+    }
+    public static String exit() {
+        return "";
+    }
 }
